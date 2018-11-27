d76c8b93ab7b1c2941c482b3ea1ba293940f1240
wsampaio <well7cordas@yahoo.com.br>
2018-11-27 16:11:53 +0000

    Semana 1 - Praticar tarefa avaliada por colega (alteração no Principal)

--- Paciente/src/Principal.java
+++ Paciente/src/Principal.java
@@ -4,8 +4,26 @@ public class Principal {
         Paciente p2 = new Paciente(105.0, 1.6);
         Paciente p3 = new Paciente(60.0, 1.9);
         
-        System.out.println(p1.diagnostico());
-        System.out.println(p2.diagnostico());
-        System.out.println(p3.diagnostico());
+        System.out.println(
+		"Paciente 1: " +
+		"Peso: " + p1.peso + " kg - " +
+		"Altura: " + p1.altura + " m - " +
+		"Diagnóstico: " + p1.diagnostico()
+	);
+
+        System.out.println(
+		"Paciente 2: " +
+		"Peso: " + p2.peso + " kg - " +
+		"Altura: " + p2.altura + " m - " +
+		"Diagnóstico: " + p2.diagnostico()
+	);
+
+        System.out.println(
+		"Paciente 3: " +
+		"Peso: " + p3.peso + " kg - " +
+		"Altura: " + p3.altura + " m - " +
+		"Diagnóstico: " + p3.diagnostico()
+	);
+
     }
 }
